834509fb5532c9204622a9c28d60520c6052cbda
Tanner <tannersiciliano@gmail.com>
2019-07-08 15:20:02 -0700

    django_heroku

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -1,5 +1,6 @@
 from .base import *
 import dj_database_url
+import django_heroku
 
 DEBUG = False
 
@@ -10,3 +11,4 @@ DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 ALLOWED_HOSTS = ['.herokuapp.com']
+django_heroku.settings(locals())
