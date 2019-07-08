200abdc1871a48ce8af880c54b54a6817781a9a6
Tanner <tannersiciliano@gmail.com>
2019-07-08 14:15:09 -0700

    Postgres

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -4,10 +4,16 @@ DEBUG = False
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('NAME'),
+        'USER': os.environ.get('USER'),
+        'PASSWORD': os.environ.get('PASSWORD'),
+        'HOST': os.environ.get('HOST'),
+        'PORT': os.environ.get('PORT'),
     }
 }
+
 ALLOWED_HOSTS = ['.herokuapp.com']
